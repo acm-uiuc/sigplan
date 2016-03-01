94ccd13b0e6956728ad7f946e09056ba1f878854
Remy Goldschmidt <taktoa@gmail.com>
2016-03-01 02:36:05 -0600

    Removed old _site, minor changes to site.hs

---
+++ src/site.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Site where
+
+import           Data.Default
+import           Data.Monoid
+import           Hakyll
+
+hconfig :: Configuration
+hconfig = def { providerDirectory = "site" }
+
+main :: IO ()
+main = hakyllWith hconfig $ do
+  match (fromList ["CNAME", "images/*"]) $ do
+    route   idRoute
+    compile copyFileCompiler
+
+  match "css/*" $ do
+    route   idRoute
+    compile compressCssCompiler
+
+  match (fromList ["about.rst", "contact.markdown"]) $ do
+    route   $ setExtension "html"
+    compile $ pandocCompiler
+      >>= loadAndApplyTemplate "templates/default.html" defaultContext
+      >>= relativizeUrls
+
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $ pandocCompiler
+      >>= loadAndApplyTemplate "templates/post.html"    postCtx
+      >>= loadAndApplyTemplate "templates/default.html" postCtx
+      >>= relativizeUrls
+
+  create ["archive.html"] $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let archiveCtx = mconcat [ listField "posts" postCtx (return posts)
+                               , constField "title" "Archives"
+                               , defaultContext ]
+
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+        >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+        >>= relativizeUrls
+
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let indexCtx = mconcat [ listField "posts" postCtx (return posts)
+                             , constField "title" "Home"
+                             , defaultContext ]
+
+      getResourceBody
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "templates/default.html" indexCtx
+        >>= relativizeUrls
+
+  match "templates/*" $ compile templateCompiler
+
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
